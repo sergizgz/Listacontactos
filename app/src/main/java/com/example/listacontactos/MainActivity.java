840aab0d2589628211c55e049e5f2b11f450b465
Sergio  Ciria Viver <sergiociriaviver@MacBook-Pro-de-Sergio.local>
2020-11-04 23:32:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/listacontactos/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.listacontactos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView texto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        texto=findViewById(R.id.texto);
+        texto.setText(String.valueOf("Hola"));
+        getNameButton();
+    }
+
+
+    public void getNameButton(){
+        try {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, PackageManager.PERMISSION_GRANTED);
+
+
+           // Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+
+
+            String[] atributos = new String[]{ContactsContract.Contacts._ID, ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.PHOTO_URI};
+
+
+
+            Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, atributos, null, null,
+                    null);
+
+
+
+            String stringContactName = "INVALID";
+            if (cursor != null){
+                if (cursor.moveToFirst()){
+                    stringContactName = cursor.getString(1);
+                }
+            }
+            texto.setText(stringContactName);
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
